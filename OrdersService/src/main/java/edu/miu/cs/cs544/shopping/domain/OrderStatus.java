d375d04e0c1491b66598262792b0099983b5f5d3
Emmanuel Niyongabo <eniyongabo@miu.edu>
2023-08-24 16:37:27 -0500

    file added

---
+++ OrdersService/src/main/java/edu/miu/cs/cs544/shopping/domain/OrderStatus.java
@@ -0,0 +1,10 @@
+package edu.miu.cs.cs544.shopping.domain;
+
+/**
+ * @author : JOHNNGUYEN
+ * @since : 5/21/2023, Sun
+ **/
+
+public enum OrderStatus {
+ NEW, PLACED, PROCESSED, SHIPPED, RETURNED
+}
