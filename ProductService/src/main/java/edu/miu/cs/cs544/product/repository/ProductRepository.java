b93753df2c946763033f4b748843afb78df799e1
eniyongabo <105735196+eniyongabo@users.noreply.github.com>
2024-01-28 21:31:42 -0600

    Update ProductRepository.java

--- ProductService/src/main/java/edu/miu/cs/cs544/product/repository/ProductRepository.java
+++ ProductService/src/main/java/edu/miu/cs/cs544/product/repository/ProductRepository.java
@@ -3,9 +3,7 @@ package edu.miu.cs.cs544.product.repository;
 import edu.miu.cs.cs544.product.domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * @author : JOHNNGUYEN
- * @since : 5/20/2023, Sat
- **/
+
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
