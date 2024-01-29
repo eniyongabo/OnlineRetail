1402a93a7e3ab1c14a81de2a7ded864d333a2915
eniyongabo <105735196+eniyongabo@users.noreply.github.com>
2024-01-28 21:27:07 -0600

    Update CustomerServiceImplTest.java

--- CustomerService/src/test/java/edu/miu/cs/cs544/customer/service/impl/CustomerServiceImplTest.java
+++ CustomerService/src/test/java/edu/miu/cs/cs544/customer/service/impl/CustomerServiceImplTest.java
@@ -6,10 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-/**
- * @author : JOHNNGUYEN
- * @since : 5/24/2023, Wed
- **/
+
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CustomerServiceImplTest {
